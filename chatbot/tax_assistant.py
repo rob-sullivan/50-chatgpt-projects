fdf926b9b12a8549db3e305e7ab9aa11c8888415
Rob Sullivan <robertosullivan5@gmail.com>
2024-01-24 22:36:33 +0000

    finished chatbot

--- chatbot/tax_assistant.py
+++ chatbot/tax_assistant.py
@@ -12,10 +12,12 @@ def index():
 @app.route("/get")
 def get_bot_response():
     instructions = """This GPT must behave answering questions related to information found on revenue.ie website, 
-    the text file contains javascript and some repeated sentences related to cookie policy, these should be ignored when providing answers, 
-    answer this as Eamon De Valera, keep his inclusion to one sentence, make it warm and witty. 
-    After answering these questions end it with 'Remember this is not advice and only for entertainment purposes, please contact 
-    a registered tax advisor or the Revenue commissioners yourself'"""
+    the text file contains javascript and some repeated sentences related to cookie policy, these should be 
+    ignored when providing answers, answer this as Eamon De Valera, keep his inclusion to one sentence, make it warm and witty. 
+    After answering these questions end it with, '<br /><br />Please be aware that while AI strives for accuracy, it can sometimes produce incorrect 
+    or misleading information. Only use this as a guide and not as the sole basis for any decisions, as we cannot accept liability 
+    for the outcomes of such decisions.' then add this below '<br /><br />If  you found this helpful then click the link to sign up for 
+    a free account and never waste time paying taxes again! <br ><br ><a href="https://forms.gle/MV3E7DMKkhSTWNxh6" target="_blank">Sign Up</a>'"""
 
     file_path = "cleaned_irish_tax_revenue_info.txt"
     with open(file_path, "r") as file:
